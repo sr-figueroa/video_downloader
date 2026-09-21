72c7b0d4b6bc1294040ee5af7810363c9990df6b
Paul Figueroa <figueroapaul86@gmail.com>
2026-09-21 18:14:39 -0500

    Simplify API_BASE_URL assignment logic

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = window.location.port === '3001' ? '' : 'http://localhost:3001';
+const API_BASE_URL = window.location.port === '';
 let currentPlatform = 'youtube';
 
 const tabs = document.querySelectorAll('.tab');
